615fb7852c3c4d8b063fd6fea69a02ef65020e6d
Bumjun-hub <jc971017@gmail.com>
2025-04-03 18:28:31 +0900

    04/03 수업

--- Ex16_Restful/src/main/java/com/study/springboot/controller/MenuRestController.java
+++ Ex16_Restful/src/main/java/com/study/springboot/controller/MenuRestController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -93,8 +94,14 @@ public class MenuRestController {
 	}
 	
 	@PostMapping()
-	public ResponseEntity<Menu> insertMenu(@RequestBody Menu menu) {
+	public ResponseEntity<?> insertMenu(@RequestBody Menu menu) {
 		Menu reMenu = menuService.insertMenu(menu);
 		return ResponseEntity.created(URI.create("/menu/" + reMenu.getId())).build();
+	}
+	
+	@PutMapping()
+	public ResponseEntity<?> updateMenu(@RequestBody Menu menu) {
+		Menu reMenu = menuService.insertMenu(menu);
+		return ResponseEntity.ok(reMenu);
 	}
 }
